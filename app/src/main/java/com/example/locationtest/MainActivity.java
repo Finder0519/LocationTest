578f75312624cb2a013ae71f4b162b7ec629cca3
SunZhenzhou <szz1285407233@163.com>
2019-06-09 21:55:00 +0800

    First commit.

---
+++ app/src/main/java/com/example/locationtest/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.locationtest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    public LocationClient mLocationClient;
+    private TextView positionText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLocationClient=new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        positionText =(TextView)findViewById(R.id.position_text_view);
+        List<String> permissionList=new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }if(!permissionList.isEmpty()){
+            String[] permissions=permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this,permissions,1);
+        }else{
+            requestLocation();
+        }
+
+    }
+    private void requestLocation(){
+        initLocation();
+        mLocationClient.start();
+    }
+    private void initLocation(){
+        LocationClientOption option=new LocationClientOption();
+        //option.setScanSpan(5000);
+        option.setIsNeedAddress(true);
+        mLocationClient.setLocOption(option);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults){
+        if(requestCode==1) {
+            if (grantResults.length > 0) {
+                for (int result : grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "需要授予权限", Toast.LENGTH_SHORT).show();
+                        finish();
+                        return;
+                    }
+                }
+                requestLocation();
+            } else {
+                Toast.makeText(this, "未知错误", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }else {
+            Toast.makeText(this, "未知错误", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    public class MyLocationListener implements BDLocationListener{
+        @Override
+        public void onReceiveLocation(BDLocation location){
+            StringBuilder currentPosition=new StringBuilder();
+            currentPosition.append("纬度：").append(location.getLatitude()).append("\n");
+            currentPosition.append("经度：").append(location.getLongitude()).append("\n");
+            currentPosition.append("国家：").append(location.getCountry()).append("\n");
+            currentPosition.append("省/自治区/直辖市：").append(location.getProvince()).append("\n");
+            currentPosition.append("地级市/自治州：").append(location.getCity()).append("\n");
+            currentPosition.append("区/县/县级市：").append(location.getDistrict()).append("\n");
+            currentPosition.append("定位方式：");
+            if(location.getLocType()==BDLocation.TypeGpsLocation){
+                currentPosition.append("GPS");
+            }else if(location.getLocType()==BDLocation.TypeNetWorkLocation) {
+                currentPosition.append("网络");
+            }
+            positionText.setText(currentPosition);
+            toastShort("您当前位于"+location.getCity()+location.getDistrict());
+        }
+    }
+    public void toastShort(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
